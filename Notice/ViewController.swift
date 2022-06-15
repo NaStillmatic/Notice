754129013c3b3e537bf2799591214dac82c7d4fe
Joe <nastillmatic@gmail.com>
2022-06-15 16:33:45 +0900

    Initial commit

---
+++ Notice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notice
+//
+//  Created by HwangByungJo  on 2022/06/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
